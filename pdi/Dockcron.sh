e2e51c942e41aaf6b7c8f684beac6afb4b976778
ROLANDO_A <rolandoazevedo@gmail.com>
2020-10-27 10:51:17 +0000

    initial commit

---
+++ pdi/Dockcron.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+Docker()
+{
+docker container run --network=rnmonitor_ipca_rnetwork --rm -v $(pwd):/jobs pdirn runj ETL/ETL_RNMONITOR.kjb
+}
+
+Checking()
+{
+checkpost=$(ps -eaf | grep postgres | grep -v grep | wc -l)
+if [[ $checkpost -ge 1 ]];then
+    echo "*******************"
+    echo "POSTGRES IS RUNNING"
+    echo "*******************"
+else
+    echo "***************************************"
+    echo "PLEASE START POSTGRES, ETL WILL NOT RUN"
+    echo "                                       "
+    echo " type docker-compose up postgres       "
+    echo "***************************************"
+    exit 1
+fi
+}
+
+Check()
+{
+Checking
+Docker
+}
+
+prog_name=${0##*/}
+
+usage=$(printf "\nPLease use the Script as:\n"; printf "\n sh $prog_name \n\n")
+
+if [[ $# -ge 1 ]];then
+     echo "${prog_name} : YOU HAVE ENTERED INCORRECT ARGUMENTS "
+     echo "${prog_name} : $usage "
+     echo "${prog_name} : EXITING PROGRAM"
+     exit
+  else
+     Check
+fi
---
+++ pdi/entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Sets script to fail if any command fails.
+set -e
+
+custom_properties() {
+	if [ -f /jobs/kettle.properties ] ; then
+		cp /jobs/kettle.properties $KETTLE_HOME
+	fi
+}
+
+run_pan() {
+	custom_properties
+	echo ./pan.sh -file $@
+	pan.sh -file /jobs/$@
+}
+
+run_kitchen() {
+	custom_properties
+	echo ./kitchen.sh -file $@
+	kitchen.sh -file /jobs/$@
+}
+
+print_usage() {
+echo "
+Usage:	$0 COMMAND
+Pentaho Data Integration (PDI)
+
+Options:
+  runj filename		Run job file
+  runt filename		Run transformation file
+  help		        Print this help
+"
+}
+
+case "$1" in
+    help)
+        print_usage
+        ;;
+    runt)
+	shift 1
+        run_pan "$@"
+        ;;
+    runj)
+	shift 1
+        run_kitchen "$@"
+        ;;
+    *)
+        exec "$@"
+esac
